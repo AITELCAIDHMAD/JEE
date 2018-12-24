660ab2e4a82af341e24c85b77b49df9269567592
DELL <DELL@192.168.1.18>
2018-12-24 11:26:13 +0000

    yet

--- TPContactsREST/src/main/java/org/glsid/ContactRepository.java
+++
@@ -1,9 +0,0 @@
-package org.glsid;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface ContactRepository extends JpaRepository<Contact, Long> {
-
-}
